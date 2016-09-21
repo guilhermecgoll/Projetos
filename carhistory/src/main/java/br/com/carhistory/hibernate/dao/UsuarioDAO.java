b56167f44be49db648d0aeeaa53432f03a362486
guilhermecgoll <guilhermecgoll@gmail.com>
2016-09-20 22:37:17 -0300

    UsuarioDAO e VeiculoDAO

---
+++ carhistory/src/main/java/br/com/carhistory/hibernate/dao/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package br.com.carhistory.hibernate.dao;
+
+import br.com.carhistory.hibernate.util.GenericDao;
+import br.com.carhistory.model.Usuario;
+
+public class UsuarioDAO extends GenericDao<Usuario, Long>{
+
+}
